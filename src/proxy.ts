777dfaf98b290e2601efa6f48f9ca6cbd0cbd952
Hadiid Andri Yulison <hadiidandri2000@gmail.com>
2025-10-24 10:05:27 +0700

    update proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,8 +4,6 @@ import { getSessionCookie } from "better-auth/cookies";
 export function proxy(request: NextRequest) {
   const sessionCookie = getSessionCookie(request);
 
-  console.log(1);
-
   if (!sessionCookie) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
@@ -14,5 +12,5 @@ export function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: "/dashboard",
+  matcher: ["/dashboard/:path*", "/tenant/:path*"],
 };
