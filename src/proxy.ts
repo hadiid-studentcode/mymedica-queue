e15cc95eec0154924f01632985080961a02fb5a8
Hadiid Andri Yulison <hadiidandri2000@gmail.com>
2025-10-25 10:27:50 +0700

    fix proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,18 +3,31 @@ import { getSessionCookie } from "better-auth/cookies";
 
 export function proxy(request: NextRequest) {
   const sessionCookie = getSessionCookie(request);
+  const isApiRoute = request.nextUrl.pathname.startsWith("/api");
 
-  if (!sessionCookie) {
+  // Jika API dan tidak ada sesi → kembalikan JSON 401
+  if (!sessionCookie && isApiRoute) {
+    return NextResponse.json(
+      { message: "Unauthorized: Session invalid or expired" },
+      { status: 401 }
+    );
+  }
+
+  // Jika Page dan tidak ada sesi → redirect ke /sign-in
+  if (!sessionCookie && !isApiRoute) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
 
   return NextResponse.next();
 }
 
-// export const config = {
-//   matcher: ["/dashboard/:path*", "/tenant/:path*","/api/tenant/:path*"],
-// };
-
 export const config = {
-  matcher: ["/dashboard/:path*", "/tenant/:path*"],
+  matcher: [
+    "/dashboard/:path*",
+    "/tenant/:path*",
+    "/api/tenant/:path*",
+    "/api/queue/:path*",
+    "/api/stage/:path*",
+    "/api/send-mail/:path*",
+  ],
 };
